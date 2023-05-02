5ff692baa2b839ed55578af8d4494cdd818ca657
Ibrahim <you@example.com>
2023-05-02 19:52:42 +0600

    images link

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 7000;
 
+const data = require('./data/chef.json')
+
+app.use(cors());
+
 app.get('/', (req, res)=>{
-    res.send('Server is Running')
+    res.send('Server is Running again and again')
+})
+
+app.get('/data', (req, res)=>{
+    res.send(data);
 })
 
 app.listen(port, ()=>{
