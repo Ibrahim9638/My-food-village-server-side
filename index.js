233d878a88f4b9b62970c209b924ff4d749087bf
Ibrahim <you@example.com>
2023-05-03 21:29:52 +0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 const data = require('./data/chef.json')
-
+const recipe = require("./data/recipeDetails.json")
 app.use(cors());
 
 app.get('/', (req, res)=>{
@@ -14,6 +14,12 @@ app.get('/', (req, res)=>{
 
 app.get('/data', (req, res)=>{
     res.send(data);
+})
+
+app.get('/recipe/:id', (req, res)=>{
+    const id = req.params.id 
+    const recipeBYChef = recipe.filter(rec=> rec.chef_id == id)
+    res.send(recipeBYChef)
 })
 
 app.listen(port, ()=>{
